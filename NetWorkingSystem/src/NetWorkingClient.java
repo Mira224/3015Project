b8510feedacb2e7cbd558aeccca93a44058611b6
Mira224 <18253008@life.hkbu.edu.hk>
2020-11-14 22:53:16 +0800

    int len =in.read(); has error when called twice

--- NetWorkingSystem/src/NetWorkingClient.java
+++ NetWorkingSystem/src/NetWorkingClient.java
@@ -6,32 +6,81 @@ import java.util.Scanner;
 
 public class NetWorkingClient {
 	static int TCPport = 9999;
+	Socket clientSocket;
+	String com = "";// which command the user require
 
 	public NetWorkingClient(String serverIP, int port) throws IOException {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Please input the IP address of Echo Server: ");
 		String server = scanner.nextLine();
 
-		Socket clientSocket = new Socket(serverIP, port);
+		clientSocket = new Socket(serverIP, port);
 		DataInputStream in = new DataInputStream(clientSocket.getInputStream());
 		DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
 
 		byte[] buffer = new byte[1024];
+		boolean login = false;
+		boolean exit = false;
 
-		System.out.print("Please input your username: ");
-		String username = scanner.nextLine();
-		System.out.print("Please input password: ");
-		String password = scanner.nextLine();
+		do {
+			System.out.print("Please input your username: ");
+			String username = scanner.nextLine();
+			System.out.print("Please input password: ");
+			String password = scanner.nextLine();
 
-		String str = "0 "+username + " " + password;
-		out.writeInt(str.length());
-		out.write(str.getBytes(), 0, str.length());
+			String str = "0 " + username + " " + password;
+			out.writeInt(str.length());
+			out.write(str.getBytes(), 0, str.length());
 
-		int len = in.readInt();
-		in.read(buffer, 0, len);
-		System.out.println(new String(buffer, 0, len));
+			int len = in.readInt();
+			in.read(buffer, 0, len);
+			String serMsg = new String(buffer, 0, len);
+			System.out.println(serMsg);
+			if (serMsg.equals("Login success")) {
+				login = true;
+				do {
+					System.out.println("Please input your option:");
+					System.out.println("1. Read file list.");
+					System.out.println("2. Create subdirectories");
+					System.out.println("3. Upload files.");
+					System.out.println("4. Download files.");
+					System.out.println("5. Delete files.");
+					System.out.println("6. Delete subdirectories.");
+					System.out.println("7. Change file/target name.");
+					System.out.println("8. Read the file¡¯s detail information");
+					System.out.println("9. logout");
+					System.out.println("10. Exit");
+					String optionNo = scanner.nextLine();
+					switch (optionNo) {
+					case "1":
+						System.out.println("Please input a directory:");
+						String msg = scanner.nextLine();
+						String req = optionNo + " " + msg;
+						out.writeInt(req.length());
+						out.write(req.getBytes(), 0, req.length());
+
+						int length = in.readInt();
+						in.read(buffer, 0, length);
+						String ser = new String(buffer, 0, length);
+						System.out.println(ser);
+						break;
+					case "9":
+						login = false;
+						System.out.println("See you~");
+						break;
+					}
+				} while (login);
+			}
+
+		} while (!login);
+
 		clientSocket.close();
 		scanner.close();
+
+	}
+
+	public void login() {
+
 	}
 
 	/*
@@ -45,7 +94,6 @@ public class NetWorkingClient {
 	 * scanMsg.nextLine(); String str = str1 + ": " + str2;
 	 * out.writeInt(str.length()); out.write(str.getBytes(), 0, str.length()); }
 	 */
-	
 
 	public void readFileList() {
 		Scanner scanner = new Scanner(System.in);
@@ -53,63 +101,11 @@ public class NetWorkingClient {
 		String filename = scanner.nextLine().trim();
 
 	}
-//
-//	public void setup() {
-//		Scanner scanner = new Scanner(System.in);
-//		System.out.println("Please input\nServer IP: ");
-//		String ip = scanner.nextLine().trim();
-//		System.out.print("Port no: ");
-//		int port = Integer.parseInt(scanner.nextLine());
-//		boolean run = true;
-//		while (run) {
-//			System.out.println("Please select the option: ");
-//			System.out.println("1. Read file list.");
-//			System.out.println("2. Create subdirectories");
-//			System.out.println("3. Upload and download files.");
-//			System.out.println("4. Delete files.");
-//			System.out.println("5. Delete subdirectories.");
-//			System.out.println("6. Change file/target name.");
-//			System.out.println("7. Read the file¡¯s detail information");
-//			System.out.println("8. Exit");
-//			String option = scanner.nextLine().trim();
-//			switch (option) {
-//			case "1":
-//				readFileList();
-//				break;
-//			// case "2":
-//			// createSubdirectories();
-//			// break;
-//			// case "3":
-//			// UploadAndDownloadFiles();
-//			// break;
-//			// case "4":
-//			// deleteFiles();
-//			// break;
-//			// case "5":
-//			// deleteSubdirectories();
-//			// break;
-//			// case "6":
-//			// changeName();
-//			// break;
-//			// case "7":
-//			// readInfor();
-//			// break;
-//			case "8":
-//				run = false;
-//				break;
-//			default:
-//				break;
-//
-//			}
-//			scanner.close();
-//		}
-//
-//	}
 
 	public static void main(String[] args) throws IOException {
-//		NetWorkingClient client = new NetWorkingClient("", 9999);
-//		client.setup();
-		new NetWorkingClient("192.168.31.238",TCPport);
+		// NetWorkingClient client = new NetWorkingClient("", 9999);
+		// client.setup();
+		new NetWorkingClient("192.168.31.238", TCPport);
 	}
 
 }
--- NetWorkingSystem/src/NetWorkingServer.java
+++ NetWorkingSystem/src/NetWorkingServer.java
@@ -4,61 +4,143 @@ import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 import java.util.Scanner;
+//import java.io.FileInputStream;
+//import java.io.FileOutputStream;
 
 public class NetWorkingServer {
- ServerSocket srvSocket;
+	ServerSocket srvSocket;
 
- public NetWorkingServer(int port) throws IOException {
+	public NetWorkingServer(int port) throws IOException {
 
-  // receiving socket from client
-  srvSocket = new ServerSocket(port);
-  byte[] buffer = new byte[1024];
-  while (true) {
-   System.out.printf("Listening at port %d...\n", port);
+		// receiving socket from client
+		srvSocket = new ServerSocket(port);
+		byte[] buffer = new byte[1024];
+		boolean whetherLogin = false;
+		boolean whetherExit = false;
+		
+			
+		while(true) {
+			System.out.printf("Listening at port %d...\n", port);
 
-   Socket clientSocket = srvSocket.accept();
+			Socket clientSocket = srvSocket.accept();
 
-   System.out.printf("Established a connection to host %s:%d\n\n", clientSocket.getInetAddress(),
-     clientSocket.getPort());
+			System.out.printf("Established a connection to host %s:%d\n\n", clientSocket.getInetAddress(),clientSocket.getPort());
 
-   DataInputStream in = new DataInputStream(clientSocket.getInputStream());
-   DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
-   int len = in.readInt();
-   in.read(buffer, 0, len);
-   
-   String command = new String(buffer, 0, len);
-   String[] com = command.split("\\s");
-   String reMsg = "";
-   
-   switch (com[0]) {
-   case "0":
-    reMsg = Login(com[1] + " " + com[2]);
-    break;
-   }
-   
-   out.writeInt(reMsg.length());
-   out.write(reMsg.getBytes(), 0, reMsg.length());
+			DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+			DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+			int len = in.readInt();
+			in.read(buffer, 0, len);
 
-   clientSocket.close();
-  }
- }
- 
- //login function
- public static String Login(String userinfo) throws IOException {
-  File user = new File("userinfo.txt");
-  Scanner u = new Scanner(user);
-  while (u.hasNext()) {
-   if (u.nextLine().equals(userinfo)) {
-    u.close();
-    return "Login success";
-   }
-  }
-  u.close();
-  return "Login failed";
- }
+			String command = new String(buffer, 0, len);
+			String[] com = command.split("\\s");
+			String reMsg = "";
 
- public static void main(String[] args) throws IOException {
-  new NetWorkingServer(9999);
- }
+			switch (com[0]) {
+			// login
+			case "0":
+				if (whetherLogin == false) {
+					if (Login(com[1] + " " + com[2])) {
+						reMsg = "Login success";
+						whetherLogin = true;
+					} else {
+						reMsg = "Login failed";
+					}
+				} else {
+					reMsg = "Already Logged in";
+				}
+				break;
+			// read file list
+			case "1":
+				reMsg = readFile(com[1]);
+				break;
+			// create sub-directories
+			case "2":
+				reMsg = createDir(com[1]);
+				
+				// upload files
+			case "3":
+				reMsg=uploadFile(com[1]);
+				//download  files
+			case "4":
+				
+			case "9":
+				whetherLogin = false;
+				break;
+			// exit
+			case "10":
+				whetherExit = true;
+				whetherLogin = false;
+				break;
+			default:
+				reMsg = "Invalid command";
+			}
+
+			out.writeInt(reMsg.length());
+			out.write(reMsg.getBytes(), 0, reMsg.length());
+			clientSocket.close();
+			
+		} 
+	}
+
+	// login function
+	public static boolean Login(String userinfo) throws IOException {
+		File user = new File("userinfo.txt");
+		Scanner u = new Scanner(user);
+		while (u.hasNext()) {
+			if (u.nextLine().equals(userinfo)) {
+				u.close();
+				return true;
+			}
+		}
+		u.close();
+		return false;
+	}
+
+	// function read files
+	public static String readFile(String path) {
+		File read = new File(path);
+		if (!read.exists()) {
+			return "Not Find The File.";
+		}
+		String reMsg = "";
+		File[] files = read.listFiles();
+		for (File f : files) {
+			if (!f.isFile()) {
+				String forFile = String.format("%s %10d %s/n", new Date(f.lastModified()), f.length(), f.getName());
+				reMsg += forFile;
+				String forDir = String.format("%s %10s %s/n", new Date(f.lastModified()), "<DIR>", f.getName());
+				reMsg += forDir;
+			}
+		}
+		return reMsg;
+	}
+
+	// function create sub-directories
+	public static String createDir(String path) {
+		File file = new File(path);
+		if (file.exists() && file.isDirectory()) {
+			return "The Directory Exists!";
+		}
+		new File(path).mkdirs();
+		
+		return "Created Successfully";
+	}
+
+	//function upload files
+	public static String uploadFile(String path) {
+		
+		return "Successfully Upload";
+	}
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) throws IOException {
+		new NetWorkingServer(9999);
+	}
 }
