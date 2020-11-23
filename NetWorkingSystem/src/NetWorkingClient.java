f882c86c52cb04442949c0e2a7c9e024cc4dd95b
Mira224 <18253008@life.hkbu.edu.hk>
2020-11-24 02:10:36 +0800

    download cannot receive return message.

--- NetWorkingSystem/src/NetWorkingClient.java
+++ NetWorkingSystem/src/NetWorkingClient.java
@@ -14,45 +14,38 @@ public class NetWorkingClient {
 	DataInputStream in;
 	DataOutputStream out;
 
-	
-
 	public NetWorkingClient(String serverIP, int port) throws IOException {
-		
 
 		clientSocket = new Socket(serverIP, port);
 		in = new DataInputStream(clientSocket.getInputStream());
 		out = new DataOutputStream(clientSocket.getOutputStream());
 
+		Thread t = new Thread(() -> {
 
-		Thread t = new Thread(() ->{
-			byte[] buffer = new byte[1024];
 			boolean login = false;
 			boolean exit = false;
 			try {
 				while (true) {
 
-					appStart(buffer,login,exit);
+					appStart(login, exit);
 				}
-				
-				
-			}catch(IOException ex) {
+
+			} catch (IOException ex) {
 				System.err.println("Connection dropped!");
 				System.exit(-1);
 			}
 		});
 		t.start();
 
-		
-
-//		out.close();
-//		clientSocket.close();
-		
+		// out.close();
+		// clientSocket.close();
 
 	}
 
-	public void appStart(byte[] buffer,boolean login, boolean exit) throws IOException {
+	public void appStart(boolean login, boolean exit) throws IOException {
 		Scanner scanner = new Scanner(System.in);
 		do {
+			byte[] buffer = new byte[1024];
 			System.out.print("Please input your username: ");
 			String username = scanner.nextLine();
 			System.out.print("Please input password: ");
@@ -69,100 +62,153 @@ public class NetWorkingClient {
 			if (serMsg.equals("Login success")) {
 				login = true;
 				do {
-					
+
 					System.out.print("Please input your command>");
-					
-						String cmd = scanner.nextLine();
-						String[] com = cmd.split("\\s+");
-						if (com[0].equalsIgnoreCase("logout")) {
-							login = false;
-							out.writeInt(cmd.length());
-							out.write(cmd.getBytes(), 0, cmd.length());
-							System.out.println();
-							int l = in.readInt();
-							in.read(buffer, 0, l);
-							String sMsg = new String(buffer, 0, l);
-							System.out.println(sMsg);
-							break;
-						} else if (com[0].equalsIgnoreCase("upload")) {//command 
-							Scanner upload = new Scanner(System.in);
-							System.out.print("Please input the upload path and filename:");
-							String uploadpath = upload.nextLine();
-							cmd = com[0] + " " + com[1] + " " + uploadpath;
-							out.writeInt(cmd.length());
-							out.write(cmd.getBytes(), 0, cmd.length());
-							upload(com[1]);
-							upload.close();
-							break;
-						} else if (com[0].equalsIgnoreCase("download")) {
-							Scanner download = new Scanner(System.in);
-							System.out.print("Please input which directory to download:");
-							String downloadpath = download.nextLine();
-							out.writeInt(cmd.length());
-							out.write(cmd.getBytes(), 0, cmd.length());
-							download(downloadpath);
-							break;
-						} else {
+					Scanner newScanner = new Scanner(System.in);
+					String cmd = newScanner.nextLine();
+					String[] com = cmd.split("\\s+");
+					if (com[0].equalsIgnoreCase("logout")) {
+						login = false;
+						out.writeInt(cmd.length());
+						out.write(cmd.getBytes(), 0, cmd.length());
+						System.out.println();
+						int l = in.readInt();
+						in.read(buffer, 0, l);
+						String sMsg = new String(buffer, 0, l);
+						System.out.println(sMsg);
+						break;
+					} else if (com[0].equalsIgnoreCase("upload")) {// command
+						upload(cmd);
 
-							out.writeInt(cmd.length());
-							out.write(cmd.getBytes(), 0, cmd.length());
+					} else if (com[0].equalsIgnoreCase("download")) {
 
-						}
+						download(cmd);
 
-						int length = in.readInt();
-						in.read(buffer, 0, length);
-						String ser = new String(buffer, 0, length);
+					} else {
+
+						out.writeInt(cmd.length());
+						out.write(cmd.getBytes(), 0, cmd.length());
+
+					}
+		
+					int length = in.readInt();
+					System.out.println("Test2");
+					while (length > 0) {
+						int blen = in.read(buffer, 0, buffer.length);
+						System.out.println("Test3");
+						String ser = new String(buffer, 0, blen);
+
 						System.out.println(ser);
-					
+						System.out.println("abc");
+						length -= blen;
+					}
+
 				} while (login);
 			}
 
 		} while (!login);
 		scanner.close();
 	}
-	
-	public void upload(String path) {
 
+	public void upload(String cmd) throws IOException {
+		Scanner upload = new Scanner(System.in);
+		System.out.print("Please input the path and filename of the file you want to upload:");
+		String uploadfile = upload.nextLine();
+
+		boolean fileExist = false;
+
+		do {
+			File fileToUpload = new File(uploadfile);
+			if (fileToUpload.exists()) {
+				if (fileToUpload.isDirectory()) {
+					System.out.print("This is a directory.");
+
+				} else {
+					fileExist = true;
+					break;
+				}
+
+			} else {
+				System.out.println(uploadfile + " does not exist.");
+			}
+			System.out.print("Please input the path and filename of the file you want to upload:");
+			uploadfile = upload.nextLine();
+
+		} while (!fileExist);
+		cmd += " " + uploadfile;
+		System.out.print("Please input where to upload with filename:");
+		String uploadpath = upload.nextLine();
+		cmd += " " + uploadpath;
+		out.writeInt(cmd.length());
+		out.write(cmd.getBytes(), 0, cmd.length());
+
+		String[] com = cmd.split("\\s+");
 		try {
 			byte[] buffer = new byte[1024];
-			File file = new File(path);
+			File file = new File(com[1]);
 			FileInputStream inFile = new FileInputStream(file);
 
 			long size = file.length();
 			out.writeLong(size);
 
-			while (in.available() > 0) {
+			while (size > 0) {
 				int len = inFile.read(buffer, 0, buffer.length);
 				out.write(buffer, 0, len);
+				size -= len;
 			}
 
-			System.out.println("Client Tranmission finished.");
+			System.out.println("Client Transmission finished.");
+
 		} catch (IOException e) {
 			System.err.println("Transmission error.");
 		}
 	}
 
-	public void download(String downloadpath) throws IOException {
+	public void download(String cmd) throws IOException {
+		Scanner download = new Scanner(System.in);
+		System.out.print("Please input the file you wish to download (path and filename):");
+		String downloadfile = download.nextLine();
+		cmd = "download " + downloadfile;
+		boolean downloadPathExist = false;
+		String downloadpath = "";
+		do {
+			System.out.print("Please input where do you wish to download the file(path ended with'/'):");
+			downloadpath = download.nextLine();
+			File downloadp = new File(downloadpath);
+
+			if (downloadp.exists()) {
+				if (downloadp.isDirectory()) {
+					downloadPathExist = true;
+
+				} else {
+					System.out.println("This is not a directory.");
+				}
+			} else {
+				System.out.println(downloadpath + " does not exist.");
+			}
+		} while (!downloadPathExist);
+
+		out.writeInt(cmd.length());
+		out.write(cmd.getBytes(), 0, cmd.length());
 		byte[] buffer = new byte[1024];
 		try {
+			DataInputStream in = new DataInputStream(clientSocket.getInputStream());
 			int nameLen = in.readInt();
 			in.read(buffer, 0, nameLen);
 			String name = new String(buffer, 0, nameLen);
-			String apath = downloadpath +"/"+ name;
-			File newfile = new File(apath);
+
+			File newfile = new File(downloadpath + name);
 			long size = in.readLong();
-			
+
 			FileOutputStream output = new FileOutputStream(newfile);
 			while (size > 0) {
 				int len = in.read(buffer, 0, buffer.length);
 				output.write(buffer, 0, len);
 				size -= len;
-//				System.out.print(size+" ");
+				// System.out.print(size+" ");
 			}
 			System.out.println("\n Client download completed.");
 
-//			in.close();
-//			output.close();
 		} catch (IOException e) {
 			System.err.println("unable to download file.");
 		}
@@ -171,9 +217,9 @@ public class NetWorkingClient {
 	public static void main(String[] args) throws IOException {
 		// NetWorkingClient client = new NetWorkingClient("", 9999);
 		// client.setup();
-		new NetWorkingClient("192.168.31.238", TCPport);
-//		new NetWorkingClient("158.182.8.145", TCPport);
-//		new NetWorkingClient("192.168.31.199", TCPport);
+		// new NetWorkingClient("192.168.31.238", TCPport);
+		// new NetWorkingClient("158.182.12.206", TCPport);
+		new NetWorkingClient("192.168.31.199", TCPport);
 	}
 
 }
--- NetWorkingSystem/src/NetWorkingServer.java
+++ NetWorkingSystem/src/NetWorkingServer.java
@@ -6,288 +6,314 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 
-
 public class NetWorkingServer {
-	ServerSocket srvSocket;
+    ServerSocket srvSocket;
+    ArrayList<Socket> list = new ArrayList<Socket>();
 
-	public NetWorkingServer(int port) throws IOException {
 
-		// receiving socket from client
-		srvSocket = new ServerSocket(port);
-		byte[] buffer = new byte[1024];
-		boolean whetherLogin = false;
-		boolean whetherExit = false;
-		DataInputStream in;
-		DataOutputStream out;
 
-		try {
-			
-			System.out.printf("Listening at port %d...\n", port);
 
-			Socket clientSocket = srvSocket.accept();
+    public NetWorkingServer(int port) throws IOException {
+        // receiving socket from client
 
-			System.out.printf("Established a connection to host %s:%d\n\n", clientSocket.getInetAddress(),
-					clientSocket.getPort());
-			in = new DataInputStream(clientSocket.getInputStream());
-			out = new DataOutputStream(clientSocket.getOutputStream());
-			
-			while (whetherExit == false) {
-				
-				int len = in.readInt();
-				in.read(buffer, 0, len);
+        srvSocket = new ServerSocket(port);
 
-				String command = new String(buffer, 0, len);
-				String[] com = command.split("\\s");
-				String reMsg = "";
+        while (true) {
+            System.out.printf("Listening at port %d...\n", port);
+            Socket clientSocket = srvSocket.accept();
 
-				switch (com[0]) {
-				// login (once only)
-				case "login":
-					if (whetherLogin == false) {
-						System.out.println("in!");
-						if (Login(com[1] + " " + com[2])) {
-							reMsg = "Login success";
-							whetherLogin = true;
-						} else {
-							reMsg = "Login failed";
-						}
-					} else {
-						reMsg = "Already Logged in";
-					}
-					break;
-				// read file list ok
-				case "dir":
-					reMsg = readFile(com[1]);
-					break;
-				// create sub-directories ok
-				case "md":
-					reMsg = createDir(com[1]);
-					break;
-					// upload files
-				case "upload":
-					try {
-						System.out.print("Downloading file " + com[2]);
-						long size = in.readLong();
-						System.out.print(" "+size);
+            synchronized (list) {
+                list.add(clientSocket);
+                System.out.printf("Total %d clients are connected.\n", list.size());
+            }
 
-						File file = new File(com[2]);
-						FileOutputStream output = new FileOutputStream(file);
-						
+            Thread t = new Thread(() -> {
+                try {
+                    serve(clientSocket);
+                } catch (IOException e) {
+                    System.err.println("connection dropped.");
+                }
+                synchronized (list) {
+                    list.remove(clientSocket);
+                }
+            });
+            t.start();
+        }
+    }
 
-						while(size > 0) {
-							int re = in.read(buffer, 0, buffer.length);
-							output.write(buffer, 0, re);
-							size -= re;
-						}
-						
-						System.out.printf("\nDownload completed."+"\n");
-						reMsg="Upload completed.";
-						
-					} catch (IOException e) {
-						System.err.println("Unable to download the file.");
-						reMsg="Unable to upload file.";
-					}
-					break;
-					
-					// download files ok
-				case "download":
-					try {
-						System.out.printf("Uploading file " + com[1]);
-						File file = new File(com[1]);
-						FileInputStream input = new FileInputStream(file);
-						out.writeInt(file.getName().length());
-						out.write(file.getName().getBytes());
-						long size = file.length();
-						out.writeLong(size);
-						while (size > 0) {
-							int read = input.read(buffer, 0, buffer.length);
-							out.write(buffer, 0, read);
-							size -= read;
-							System.out.println(size);
-						}
-					} catch (IOException e) {
-						System.err.println("Unable to upload file.");
-						reMsg="Unable to download the file.";
-					}
-					
-					break;
-					
-				// delete file ok
-				case "del":
-					reMsg = delFile(com[1]);
-					break;
-				// delete sub-directory ok
-				case "rd":
-					reMsg = delDir(com[1]);
-					break;
-				// change file/target name ok
-				case "ren":
-					reMsg = rename(com[1], com[2]);
-					break;
-				// read files details ok
-				case "readDe":
-					reMsg=shwDetail(com[1]);
-					break;
-				// logout
-				case "logout":
-					reMsg="Good bye.";
-					whetherLogin = false;
-					break;
-				// exit
-				case "exit":
-					reMsg="Bye~";
-					whetherExit = true;
-					whetherLogin = false;
-					break;
-				default:
-					reMsg = "Invalid command";
-				}
+    public void serve(Socket clientSocket) throws IOException {
+        boolean whetherLogin = false;
+        boolean whetherExit = false;
 
-				out.writeInt(reMsg.length());
-				out.write(reMsg.getBytes(), 0, reMsg.length());
-				
-			}
-			in.close();
-			out.close();
-			clientSocket.close();
-		}catch(IOException e) {
-			System.out.printf("Failed to connect port %d...\\n",port);
-			
-		}
-		
+        System.out.printf("Established a connection to host %s:%d\n\n", clientSocket.getInetAddress(),
+                clientSocket.getPort());
 
-	}
+        while (!whetherExit) {
+            DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+            DataOutputStream out= new DataOutputStream(clientSocket.getOutputStream());
+            int len = in.readInt();
+            byte[] buffer = new byte[len];
+            in.read(buffer, 0, len);
 
-	// login function
-	public boolean Login(String userinfo) throws IOException {
-		File user = new File("userinfo.txt");
-		Scanner u = new Scanner(user);
-		while (u.hasNext()) {
-			if (u.nextLine().equals(userinfo)) {
-//				u.close();
-				return true;
-			}
-		}
-//		u.close();
-		return false;
-	}
+            String command = new String(buffer);
+            System.out.println(command);
+            String[] com = command.split("\\s");
+            String reMsg = "";
 
-	// function read files ok
-	public  String readFile(String path) {
-		File read = new File(path);
-		if (!read.exists()) {
-			return "Not Find The File.";
-		}
-		String reMsg = "";
-		File[] files = read.listFiles();
-		for (File f : files) {
-			if (!f.isFile()) {
-				String forDir = String.format("%s %10s %s\n", new Date(f.lastModified()), "<DIR>", f.getName());
-				reMsg += forDir;
-			}else{
-				String forFile = String.format("%s %10d %s\n", new Date(f.lastModified()), f.length(), f.getName());
-				reMsg += forFile;
-			}
-		}
-		return reMsg;
-	}
+            switch (com[0]) {
+                // login (once only)
+                case "login":
+                    if (whetherLogin == false) {
+                        System.out.println("Client logged in!");
+                        if (Login(com[1] + " " + com[2])) {
+                            reMsg = "Login success";
+                            whetherLogin = true;
+                        } else {
+                            reMsg = "Login failed";
+                        }
+                    } else {
+                        reMsg = "Already Logged in";
+                    }
+                    break;
+                // read file list
+                case "dir":
+                    reMsg = readFile(com[1]);
+                    break;
+                // create sub-directories
+                case "md":
+                    reMsg = createDir(com[1]);
+                    break;
+                // upload files
+                case "upload":
+                    try {
 
-	// function create sub-directories ok
-	public  String createDir(String path) {
-		File create = new File(path);
-		if (create.exists() && create.isDirectory()) {
-			return "The Directory Exists!";
-		}
-		new File(path).mkdirs();
+                        long size = in.readLong();
+                        System.out.println(size);
+                        File file = new File(com[2]);
+                        if(file.exists()) {
+                            if (file.isFile()) {
+                                reMsg = "File " + com[2] + " has already exist.";
+                                break;
+                            }
+                        }else {
+                            System.out.print("Downloading file " + com[2]);
+                            FileOutputStream output = new FileOutputStream(file);
 
-		return "Created Successfully";
-	}
-	
-	// function delete file ok
-	public String delFile(String path) {
-		File delf = new File(path);
-		if (delf.exists()) {
-			if (delf.isDirectory()) {
-				return "To delete a directory, use RD command.";
-			}
-			new File(path).delete();
-			return "Successful delete" + path;
-		}
-		return "Cannot Find File " + path;
-	}
+                            while (size > 0) {
+                                int re = in.read(buffer, 0, buffer.length);
+                                output.write(buffer, 0, re);
+                                size -= re;
+                            }
 
-	// function delete sub-directory ok
-	public String delDir(String path) {
-		File dir = new File(path);
-		if (dir.exists()) {
-			if (dir.isFile()) {
-				return "To delete a directory, use DEL command.";
-			}
-			File[] f = dir.listFiles();
-			if (f.length != 0) {
-				return "The Directory " + path + " Is Not Empty!";
-			}
-			new File(path).delete();
-			return "Successful delete" + path;
-		}
-		return "Cannot Find Directory \"+path";
+                            System.out.printf("\nDownload completed." + "\n");
+                            reMsg = "Upload completed.";
+                        }
+                    } catch (IOException e) {
+                        System.err.println("Unable to download the file.");
+                        reMsg = "Unable to upload file.";
+                    }
+                    break;
 
-	}
+                // download files
+                case "download":
+                    try {
+                        File file = new File(com[1]);
+                        if(file.exists()) {
+                            if(file.isFile()) {
+                                System.out.printf("Uploading file " + com[1]);
+                                FileInputStream input = new FileInputStream(file);
+                                out.writeInt(file.getName().length());
+                                out.write(file.getName().getBytes());
+                                long size = file.length();
+                                out.writeLong(size);
+                                while (size > 0) {
+                                    int read = input.read(buffer, 0, buffer.length);
+                                    out.write(buffer, 0, read);
+                                    size -= read;
+                                }
+                                System.out.println("Success upload.");
+                                reMsg = "Download success.";
+                            }else {
+                                reMsg = com[1] + "is not a file.";
+                            }
+                        }else{
+                            reMsg=com[1]+" not exist.";
+                        }
+                    } catch (IOException e) {
+                        System.err.println("Unable to upload file.");
+                        reMsg = "Unable to download the file.";
+                    }
+                    break;
 
-	// function rename file ok
-		public String rename(String path, String filename) throws IOException {
-			File rename = new File(path);
-			String newFileName = rename.getParent() + "/"+ filename;
-			File newFile = new File(newFileName);
-			
-			if(rename.exists()) {
-				if (rename.isDirectory()) {
-					return "This is not a file";
-				}else if(rename.isFile()) {
-					boolean re = rename.renameTo(newFile);
-					if (!re) {
-						return  "Fail to rename file "+path;
-					}else {
-						return "Rename compeleted.";
-					}
-				}
-			}
+                // delete file fine
+                case "del":
+                    reMsg = delFile(com[1]);
+                    break;
+                // delete sub-directory fine
+                case "rd":
+                    reMsg = delDir(com[1]);
+                    break;
+                // change file/target name fine
+                case "ren":
+                    reMsg = rename(com[1], com[2]);
+                    break;
+                // read files details fine
+                case "readDe":
+                    reMsg = shwDetail(com[1]);
+                    break;
+                // logout
+                case "logout":
+                    reMsg = "Good bye.";
+                    whetherLogin = false;
+                    break;
+                // exit
+                case "exit":
+                    reMsg = "Bye~";
+                    whetherExit = true;
+                    whetherLogin = false;
+                    break;
+                default:
+                    reMsg = "Invalid command";
+            }
 
-			return "File "+path+" is not exist.";
-		}
+            out.writeInt(reMsg.length());
+            out.write(reMsg.getBytes(), 0, reMsg.length());
 
-	// function show file details ok
-	public String shwDetail(String path) throws IOException {
-		File show=new File(path);
-		String str1="name : " + show.getName()+"\n";
-		String str2="size (bytes) : " + show.length()+"\n";
-		String str3="absolute path? : " + show.isAbsolute()+"\n";
-		String str4="exists? : " + show.exists()+"\n";
-		String str5="hidden? : " + show.isHidden()+"\n";
-		String str6="dir? : " + show.isDirectory()+"\n";
-		String str7="file? : " + show.isFile()+"\n";
-		String str8="modified (timestamp) : " + show.lastModified()+"\n";
-		String str9="readable? : " + show.canRead()+"\n";
-		String strA="writable? : " + show.canWrite()+"\n";
-		String strB="executable? : " + show.canExecute()+"\n";
-		String strC="parent : " + show.getParent()+"\n";
-		String strD="absolute file : " + show.getAbsoluteFile()+"\n";
-		String strE="absolute path : " + show.getAbsolutePath()+"\n";
-		String strF="canonical file : " + show.getCanonicalFile()+"\n";
-		String strG="canonical path : " + show.getCanonicalPath()+"\n";
-		String strH="partition space (bytes) : " + show.getTotalSpace()+"\n";
-		String strI="usable space (bytes) : " + show.getUsableSpace()+"\n";
-		
-		return str1+str2+str3+str4+str5+str6+str7+str8+str9+strA+strB+strC+strD+strE+strF+strG+strH+strI;
-	}
+        }
+    }
 
-	public static void main(String[] args) throws IOException {
-		int tcpPort=9999;
-		int udpPort=9998;
-		new NetWorkingServer(tcpPort);
-		
-	}
+    // login function
+    public boolean Login(String userinfo) throws IOException {
+        File user = new File("userinfo.txt");
+        Scanner u = new Scanner(user);
+        while (u.hasNext()) {
+            if (u.nextLine().equals(userinfo)) {
+                // u.close();
+                return true;
+            }
+        }
+        // u.close();
+        return false;
+    }
+
+    // function read files fine
+    public String readFile(String path) {
+        File read = new File(path);
+        if (!read.exists()) {
+            return "Not Find The File.";
+        }
+        String reMsg = "";
+        File[] files = read.listFiles();
+        for (File f : files) {
+            if (!f.isFile()) {
+                String forDir = String.format("%s %10s %s\n", new Date(f.lastModified()), "<DIR>", f.getName());
+                reMsg += forDir;
+            } else {
+                String forFile = String.format("%s %10d %s\n", new Date(f.lastModified()), f.length(), f.getName());
+                reMsg += forFile;
+            }
+        }
+        return reMsg;
+    }
+
+    // function create sub-directories fine
+    public String createDir(String path) {
+        File create = new File(path);
+        if (create.exists() && create.isDirectory()) {
+            return "The Directory Exists!";
+        }
+        new File(path).mkdirs();
+
+        return "Created Successfully";
+    }
+
+    // function delete file
+    public String delFile(String path) {
+        File delf = new File(path);
+        if (delf.exists()) {
+            if (delf.isDirectory()) {
+                return "To delete a directory, use RD command.";
+            }
+            new File(path).delete();
+            return "Successful delete" + path;
+        }
+        return "Cannot Find File " + path;
+    }
+
+    // function delete sub-directory
+    public String delDir(String path) {
+        File dir = new File(path);
+        if (dir.exists()) {
+            if (dir.isFile()) {
+                return "To delete a directory, use DEL command.";
+            }
+            File[] f = dir.listFiles();
+            if (f.length != 0) {
+                return "The Directory " + path + " Is Not Empty!";
+            }
+            new File(path).delete();
+            return "Successful delete" + path;
+        }
+        return "Cannot Find Directory \"+path";
+
+    }
+
+    // function rename file
+    public String rename(String path, String filename) throws IOException {
+        File rename = new File(path);
+        String newFileName = rename.getParent() + "/" + filename;
+        File newFile = new File(newFileName);
+
+        if (rename.exists()) {
+            if (rename.isDirectory()) {
+                return "This is not a file";
+            } else if (rename.isFile()) {
+                boolean re = rename.renameTo(newFile);
+                if (!re) {
+                    return "Fail to rename file " + path;
+                } else {
+                    return "Rename compeleted.";
+                }
+            }
+        }
+
+        return "File " + path + " is not exist.";
+    }
+
+    // function show file details
+    public String shwDetail(String path) throws IOException {
+        File show = new File(path);
+        String str1 = "name : " + show.getName() + "\n";
+        String str2 = "size (bytes) : " + show.length() + "\n";
+        String str3 = "absolute path? : " + show.isAbsolute() + "\n";
+        String str4 = "exists? : " + show.exists() + "\n";
+        String str5 = "hidden? : " + show.isHidden() + "\n";
+        String str6 = "dir? : " + show.isDirectory() + "\n";
+        String str7 = "file? : " + show.isFile() + "\n";
+        String str8 = "modified (timestamp) : " + show.lastModified() + "\n";
+        String str9 = "readable? : " + show.canRead() + "\n";
+        String strA = "writable? : " + show.canWrite() + "\n";
+        String strB = "executable? : " + show.canExecute() + "\n";
+        String strC = "parent : " + show.getParent() + "\n";
+        String strD = "absolute file : " + show.getAbsoluteFile() + "\n";
+        String strE = "absolute path : " + show.getAbsolutePath() + "\n";
+        String strF = "canonical file : " + show.getCanonicalFile() + "\n";
+        String strG = "canonical path : " + show.getCanonicalPath() + "\n";
+        String strH = "partition space (bytes) : " + show.getTotalSpace() + "\n";
+        String strI = "usable space (bytes) : " + show.getUsableSpace() + "\n";
+
+        return str1 + str2 + str3 + str4 + str5 + str6 + str7 + str8 + str9 + strA + strB + strC + strD + strE + strF
+                + strG + strH + strI;
+    }
+
+    public static void main(String[] args) throws IOException {
+        int tcpPort = 9999;
+        int udpPort = 9998;
+        new NetWorkingServer(tcpPort);
+
+    }
 }
